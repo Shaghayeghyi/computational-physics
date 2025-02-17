29ed899463becc50e2c9e293134f3623d7c4d467
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-02-17 12:48:17 +0330

    we have used nested loops to redo our process

--- HW1/Question 1/Koch fractal.py
+++ HW1/Question 1/Koch fractal.py
@@ -9,12 +9,12 @@ pf=np.array([1,0])
 
 #splitting it in 3 segments!
 #pi is the start point and pf is the end of the line therefore p1=pi+1/3(pf-pi) and p2=pi+2/3(pf-pi)
-p1=pi+1/3*(pf-pi)
+'''p1=pi+1/3*(pf-pi)
 p2=pi+2/3*(pf-pi)
-'''print(p1,p2)'''
+print(p1,p2)
 
 #now i should split the middle part in two!
-pm=(p2-p1)/2
+pm=(p2-p1)/2'''
 
 #the two lines p1-pm and pm-p2 should rotate 60 degrees
 #I should create a rotation function for me to do this.
@@ -28,11 +28,52 @@ def rotation(R_end, R_origin, tetha):
     #lets do the rotation
     return (np.dot(R,l)+R_origin)
 
-R1=rotation(p2,p1,60)
-R2=rotation(pm,p2,-60)
-plot_data=np.array([pi,p1,R1,p2,pf])
+#now that we have an algorithm lets make some for loops to repeat this code for each new segment and create a koch curve:)
+#for doing so, we fisrt should have a iteration number so we will stop the algorithm at some point. if the iteration steps is 0 then we are left
+#with pi and pf only
 
+def fractal(pi,pf,iteration):
+    #we have an aray of 2D point that we want to draw.
+    points=[]
+    if iteration==0:
+        #no changes
+        points=[pi,pf]
+    else:
+        points=[pi,pf]
+        for n in range(iteration):
+            #this is to keep every point sorted correctly
+            new_points=[]
+            #now we will have some new points by each step.
+            #for example in the first step we will create three new points
+            #we need these points to create the base line for the ;next; step
+            for m in range(len(points)-1):
+                #for two point we have one line for 5 points we have 4 line and..
+                origin=points[m]
+                finish=points[m+1]
+                #each line is created with two adjacent points
+                #for each line we made three segments within
+                p1=origin+1/3*(finish-origin)
+                p2=origin+2/3*(finish-origin)
+                #now the 60 degree rotated part
+                #the order is important for us or maybe we can use some sort algorithm and do not bother!
+                R=rotation(p2,p1,60)
+                new_points.append(origin)
+                new_points.append(p1)
+                new_points.append(R)
+                new_points.append(p2)
+            new_points.append(finish)
+            points=new_points
+    return points                
+                
+            
+'''R1=rotation(p2,p1,60)
+R2=rotation(pm,p2,-60)'''
+#now we will draw all of the sorted points we have
+plot_data=np.array(fractal(pi,pf,2))
 plt.figure(figsize=(6,6))
 plt.plot(plot_data[:,0],plot_data[:,1])
+#this little guy helped me with axis symmetry :)
+plt.axis('equal')
+plt.title('snowflake!')
 plt.legend()
 plt.show()
