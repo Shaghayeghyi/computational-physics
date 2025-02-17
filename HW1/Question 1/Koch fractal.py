05b21b1ebf8e643ce430d2b99561473573555e32
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-02-17 10:57:12 +0330

    trying to creat the very fisrt step of koch by rotation and translation

--- HW1/Question 1/Koch fractal.py
+++ HW1/Question 1/Koch fractal.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#This is two D plot. so wil create a line with a specific lenght using 2 D array
+#This is two D plot. so we will create a line with a specific lenght using 2 D array
 #The initial line
 pi=np.array([0,0])
 pf=np.array([1,0])
@@ -17,3 +17,22 @@ p2=pi+2/3*(pf-pi)
 pm=(p2-p1)/2
 
 #the two lines p1-pm and pm-p2 should rotate 60 degrees
+#I should create a rotation function for me to do this.
+
+def rotation(R_end, R_origin, tetha):
+    degree=np.radians(tetha)
+    #define a the typical rotation matrix for a 2D space :)
+    R=np.array([[np.cos(degree), -np.sin(degree)],[np.sin(degree),  np.cos(degree)]])
+    #alright now we define our line of interest
+    l=R_end-R_origin
+    #lets do the rotation
+    return (np.dot(R,l)+R_origin)
+
+R1=rotation(p2,p1,60)
+R2=rotation(pm,p2,-60)
+plot_data=np.array([pi,p1,R1,p2,pf])
+
+plt.figure(figsize=(6,6))
+plt.plot(plot_data[:,0],plot_data[:,1])
+plt.legend()
+plt.show()
