536ef15770f65b98d3c225cafda14dc0ac35a22d
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-02-19 10:52:30 +0330

    creating step1 in two colors to test the code

--- HW1/Question 2/Dragon.py
+++ HW1/Question 2/Dragon.py
@@ -39,8 +39,8 @@ def R_45(l0):
     degree=np.radians(tetha)
     #define a the typical rotation matrix for a 2D space :)
     Ro=(np.array([[np.cos(degree), -np.sin(degree)],[np.sin(degree),  np.cos(degree)]]))*1/m.sqrt(2)
-    pi_new=np.dot(Ro,pi)
-    pf_new=np.dot(Ro,pf)
+    pi_new=np.dot(Ro,l0[0])
+    pf_new=np.dot(Ro,l0[1])
     #new line
     l1=np.array([pi_new,pf_new])
     return l1
@@ -51,15 +51,24 @@ def RT_135(l0):
     T=np.array([1,0])
     #define a the typical rotation matrix for a 2D space :)
     Ro=(np.array([[np.cos(degree), -np.sin(degree)],[np.sin(degree),  np.cos(degree)]]))*1/m.sqrt(2)
-    pi_new2=np.dot(Ro,pi)+T
-    pf_new2=np.dot(Ro,pf)+T
+    pi_new2=np.dot(Ro,l0[0])+T
+    pf_new2=np.dot(Ro,l0[1])+T
     #we have line with 2 new endpoints
-    l2=np.array([pi_new2,pf_new2])
+    l2=np.array([pf_new2,pi_new2])
     return l2
 
 #I should also think about lines as in we need two endpoints for knowing it excatly. so i should create another array to represent a line
 line0=np.array([pi,pf])
 #this is now 2x2 the column 0 are the x and the column 1 is the y as usual:)
 #the functions with get line0 and give back the two lines of step 1, then we should apply the functions on our new lines and go on
-
-    
+l1=R_45(line0)
+l2=RT_135(line0)
+print(l1)
+print(l2)
+data_plot=np.array([l1,l2])
+plt.figure(figsize=(6,6))
+plt.plot(l1[:,0],l1[:,1], c='b')
+plt.plot(l2[:,0],l2[:,1], c='r')
+#this little guy helped me with axis symmetry :)
+plt.axis('equal')
+plt.show()  
