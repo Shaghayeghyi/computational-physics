fa383a94fe272fdf0cf6f57302170e0d0b4a83be
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-02-19 18:18:35 +0330

    creating the logic of pascal triangle

---
+++ HW1/Question 4/Pascal.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+
+#at first we need to create the pascal triangle
+#in the pascal triangle each number is a polynomial factor
+#such that each number is (n,k) were n is the row and k is the position both starting from 0
+#we also notice that each row like i has i+1 positions for numbers
+# then n as the number of row is our arbitrary parameter
+#we also has two loops
+n=5
+for i in range(n):
+    for j in range(i+1):
+        #at row i and position j
+        #we have n rows and i+1 number on each row
+        number=m.factorial(i) // (m.factorial(j) * m.factorial(i - j))
+        print(number)
