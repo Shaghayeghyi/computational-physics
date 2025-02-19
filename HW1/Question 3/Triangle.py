bd0d56f543b46df633747ab8824dc07b1caea7d2
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-02-19 17:00:33 +0330

    change of plan, working with 3 points together

--- HW1/Question 3/Triangle.py
+++ HW1/Question 3/Triangle.py
@@ -11,6 +11,8 @@ p2=np.array([1,0])
 p3=np.array([1/2,m.sqrt(3)/2])
 #get the set of coordinates to describe the T
 tr=np.array([p1,p3,p2])
+'''T3=np.array([1/2,0])
+print(tr+T3)'''
 
 #i believe everything is very similar to the last code
 #i need to work with three lines and i also need to work with three functions
@@ -25,35 +27,23 @@ colored_T= plt.Polygon(tr, color='blue')
 ax.add_patch(colored_T)
 plt.show()
 
-# Optionally, plot the outline of the triangle
-ax.plot(tr[:, 0], tr[:, 1], 'k-', linewidth=2)
-def scale(l0):
-    #scale by half
-    S=np.array([[1/2,0],[0,1/2]])
-    pi_new=np.dot(S,l0[0])
-    pf_new=np.dot(S,l0[1])
-    l1=np.array([pi_new,pf_new])
-    return l0
 
-def Transfer(l1):
+
+#it is not efficient to work with three lines. i will male the 3 point a single entity!
+#my mind was stuck in the Dragon curve :))
+def scale(tr):
+    #scale by half
+    return tr/2
+def Transfer(l0):
     #scaling
-    S=np.array([[1/2,0],[0,1/2]])
     #one direction transfer
     T=np.array([1/2,0])
-    pi_new=np.dot(S,l1[0])+T
-    pf_new=np.dot(S,l1[1])+T
-    l1=np.array([pi_new,pf_new])
-    return l1
-def Transfer2(l2):
+    return tr/2 +T
+def Transfer2(l0):
     #scaling
-    S=np.array([[1/2,0],[0,1/2]])
     #one direction transfer
-    T=np.array([1/4,m.sqrt(3)/4])
-    pi_new=np.dot(S,l1[0])+T
-    pf_new=np.dot(S,l1[1])+T
-    l2=np.array([pi_new,pf_new])
-    return l2
-
+    T2=np.array([1/4,m.sqrt(3)/4])
+    return tr+T2
 
 
 
