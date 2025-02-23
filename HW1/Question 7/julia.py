8fa115d31ca47d0314437cb09bb08674bfc94f98
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-02-23 10:18:45 +0330

    change of plan to work with pixels inside an array

--- HW1/Question 7/julia.py
+++ HW1/Question 7/julia.py
@@ -15,7 +15,7 @@ import math as m
 y=random.uniform(0,10)
 z=complex(x,y)
 print(Ncomplex)'''
-#boundary distance
+'''#boundary distance
 r=2
 
 N_iteration=[]
@@ -45,13 +45,45 @@ for i in range(N_randoms):
     point=np.array([x,y,l])
     data.append(point)
 data=np.array(data)
-'''#just checking
-print(data[:,1])'''
+#just checking
+print(data[:,1])
 
-'''colors = plt.cm.viridis(np.linspace(0, 1, Max))
-print(colors)'''
+colors = plt.cm.viridis(np.linspace(0, 1, Max))
+print(colors)
 plt.figure(figsize=(8, 8))
 plt.scatter(data[:, 0], data[:, 1], c=data[:, 2], cmap='plasma',s=0.5)
-plt.show()
+plt.show()'''
+
+
+N_randoms=50000
+#Max iteration number of function
+Max=50
+
+#for creating a better plot maybe i should work with the pixels instead of points.
+#i will have a plane in this limit (xi,xf) and (yi,yf)
+xi,xf=-10,10
+yi,yf=-10,10
+r=2
+#each number is a pixel in my mind so at first i will create a 2D array 20x20
+initial_value=np.zeros((20,20))
+def z_function(z):
+    c=complex(-0.4,-0.6)
+    f=z**2+c
+    return f
+#now i will create random numbers in this plane and assign a new number l instead of zero to each pixel
+
+for i in range(N_randoms):
+    #to cover the whole parts of page
+    x=random.uniform(-10,10)
+    y=random.uniform(-10,10)
+    z=complex(x,y)
+    #counter
+    l=0
+    #for all iterations
+    while abs(z)<=r and l<=Max:
+        z=z_function(z)
+        l=l+1
+    initial_value[x,y]=l
+
 
 
