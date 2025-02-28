630ce670ef19850d9f5ba90a6bb22ea50e0d05a1
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-01 03:25:34 +0330

    plot and fit for 4 snapshots

--- HW2/Question 1/velneshast.py
+++ HW2/Question 1/velneshast.py
@@ -18,13 +18,13 @@ time_splitted_sample2=[]
 
 
 for i in range(N_samples):
-    if i==int(N_samples/4):
+    if i==int(N_samples//4):
         time_splitted_sample.append(height.copy())
         time_splitted_sample2.append(height.copy())
-    if i==2*int(N_samples/4):
+    if i==2*int(N_samples//4):
         time_splitted_sample.append(height.copy()- time_splitted_sample[0])
         time_splitted_sample2.append(height.copy())
-    if i==3*int(N_samples/4):
+    if i==3*int(N_samples//4):
         time_splitted_sample.append(height.copy()- time_splitted_sample[0]-time_splitted_sample[1])
         time_splitted_sample2.append(height.copy())
     if i==N_samples-1:
@@ -38,11 +38,33 @@ for i in range(N_samples):
     
     
 #print(x)
-plt.bar(x,time_splitted_sample[0], color='blue',alpha=0.6)
+'''plt.bar(x,time_splitted_sample[0], color='blue',alpha=0.6)
 plt.bar(x,time_splitted_sample[1], color='red',alpha=0.6,bottom=np.sum(time_splitted_sample[:1], axis=0))
 plt.bar(x,time_splitted_sample[2], color='blue',alpha=0.6,bottom=np.sum(time_splitted_sample[:2], axis=0))
 plt.bar(x,time_splitted_sample[3], color='red',alpha=0.6,bottom=np.sum(time_splitted_sample[:3], axis=0))
 
-plt.show()
+plt.show()'''
 #so the time_splitted_sample contains the lenght of each snapshot on deposition of our surface
-#mean height generl formula
+#mean height general formula
+mean_height_snapshot=[]
+for j in range(len(time_splitted_sample2)):
+    mean=np.mean(time_splitted_sample2[j])
+    mean_height_snapshot.append(float(mean))
+#print(mean_height_snapshot)    
+#var
+w=[]
+for l in range(len(time_splitted_sample2)):
+    var=np.sqrt(np.var(time_splitted_sample2[l]))
+    w.append(float(var))
+time=[int(N_samples//4),2*int(N_samples//4),3*int(N_samples//4),N_samples-1]
+#print(w)
+plt.plot(np.log(time), np.log(w), marker='o')
+
+print('hello')
+#beta
+
+
+b,a= np.polyfit(np.log(time), np.log(w), 1)
+print(b)
+plt.show()
+
