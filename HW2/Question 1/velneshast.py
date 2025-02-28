177dce43ae545fe930dd8fc738fa85ff39f8ffff
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-02-28 16:48:16 +0330

    trying to plot different colors

--- HW2/Question 1/velneshast.py
+++ HW2/Question 1/velneshast.py
@@ -9,11 +9,19 @@ x=np.arange(0,200,1)
 #this is a 1D array to save the height which is 0 at first
 height=np.zeros(200)
 #we will also need a number of samples that will fall on the surface
-N_sample=20000
+N_samples=20000
 
 #we need a for loop to go through every sample and find a place for it
 time_splitted_sample=[]
-for i in range(N_sample):
+for i in range(N_samples):
+    if i==int(N_samples/4):
+        time_splitted_sample.append(height.copy())
+    if i==2*int(N_samples/4):
+        time_splitted_sample.append(height.copy())
+    if i==3*int(N_samples/4):
+        time_splitted_sample.append(height.copy())
+    if i==N_samples-1:
+        time_splitted_sample.append(height.copy())
     #the question wants us to split our deposition into 4 differen timelines
     #so we will save each quarter of samples in a array
     rand_house=r.randint(0,199)
@@ -22,5 +30,9 @@ for i in range(N_sample):
     
     
 #print(x)
-plt.bar(x,height)
+plt.bar(x,time_splitted_sample[0], color='blue',alpha=0.6)
+plt.bar(x,time_splitted_sample[1], color='red',alpha=0.6)
+plt.bar(x,time_splitted_sample[2], color='blue',alpha=0.6)
+plt.bar(x,time_splitted_sample[3], color='red',alpha=0.6)
+
 plt.show()
