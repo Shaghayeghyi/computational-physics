4c419bee1bc24645c6baf7fe854ef5d513e456a0
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-01 10:49:49 +0330

    figured the slope

--- HW2/Question 1/velneshast.py
+++ HW2/Question 1/velneshast.py
@@ -9,7 +9,7 @@ x=np.arange(0,200,1)
 #this is a 1D array to save the height which is 0 at first
 height=np.zeros(200)
 #we will also need a number of samples that will fall on the surface
-N_samples=20000
+N_samples=200000
 
 #we need a for loop to go through every sample and find a place for it
 time_splitted_sample=[]
@@ -17,7 +17,7 @@ time_splitted_sample=[]
 time_splitted_sample2=[]
 
 
-for i in range(N_samples):
+'''for i in range(N_samples):
     if i==int(N_samples//4):
         time_splitted_sample.append(height.copy())
         time_splitted_sample2.append(height.copy())
@@ -38,33 +38,63 @@ for i in range(N_samples):
     
     
 #print(x)
-'''plt.bar(x,time_splitted_sample[0], color='blue',alpha=0.6)
+plt.bar(x,time_splitted_sample[0], color='blue',alpha=0.6)
 plt.bar(x,time_splitted_sample[1], color='red',alpha=0.6,bottom=np.sum(time_splitted_sample[:1], axis=0))
 plt.bar(x,time_splitted_sample[2], color='blue',alpha=0.6,bottom=np.sum(time_splitted_sample[:2], axis=0))
 plt.bar(x,time_splitted_sample[3], color='red',alpha=0.6,bottom=np.sum(time_splitted_sample[:3], axis=0))
 
 plt.show()'''
+
+
+
+
+
+
+
+
+
 #so the time_splitted_sample contains the lenght of each snapshot on deposition of our surface
 #mean height general formula
+#we also can save the height in more times
+height_snapshot=[]
+#for counter!
+time_snapshot=[100,1000,10000,100000, N_samples-1]
+h=np.zeros(200)
+for m in range(N_samples):
+   
+    rand=r.randint(0,199)
+    #add a unit to the height of rand_house
+    h[rand]=h[rand]+1
+    if np.isin(m, time_snapshot):
+        height_snapshot.append(h.copy())
+
+       
 mean_height_snapshot=[]
-for j in range(len(time_splitted_sample2)):
-    mean=np.mean(time_splitted_sample2[j])
+for j in range(len(height_snapshot)):
+    mean=np.mean(height_snapshot[j])
     mean_height_snapshot.append(float(mean))
-#print(mean_height_snapshot)    
+print(mean_height_snapshot)    
 #var
 w=[]
-for l in range(len(time_splitted_sample2)):
-    var=np.sqrt(np.var(time_splitted_sample2[l]))
+for l in range(len(height_snapshot)):
+    var=np.sqrt(np.mean((height_snapshot[l])**2)-(np.mean(height_snapshot[l]))**2)
     w.append(float(var))
-time=[int(N_samples//4),2*int(N_samples//4),3*int(N_samples//4),N_samples-1]
-#print(w)
-plt.plot(np.log(time), np.log(w), marker='o')
+#time=[int(N_samples//4),2*int(N_samples//4),3*int(N_samples//4),N_samples-1]
+print(w)
 
-print('hello')
+#print('hello')
 #beta
 
 
-b,a= np.polyfit(np.log(time), np.log(w), 1)
+b,a= np.polyfit(np.log(time_snapshot), np.log(w), 1)
+plt.plot(np.log(time_snapshot), np.log(w), marker='o', label=f"Slope= {b}")
+plt.title("w and t for 200000 samples")
+plt.xlabel("logt")
+plt.ylabel("logw")
+plt.legend(loc="upper left")
+
+
+
 print(b)
 plt.show()
 
