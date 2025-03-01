47acbb5280a2d769dbfb9c3add29732fea501ba1
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-01 13:04:32 +0330

    saved snap shots of height

--- HW2/Question 2/payin.py
+++ HW2/Question 2/payin.py
@@ -3,11 +3,11 @@ import random as r
 import matplotlib.pyplot as plt
 #now we will work with some interaction with neighbors
 #again we have a 1D surface
-x=np.arange(0,200,1)
+'''x=np.arange(0,200,1)
 #for each house we need to determine the height
 height=np.zeros(200)
 #we also need a time_snap to save the dynamic after a certain amount of time
-N_samples=200000
+N_samples=20000
 height_snapshot=[]
 time_snapshot=[int(N_samples/4),2*int(N_samples/4),3*int(N_samples/4),N_samples-1]
 
@@ -43,5 +43,44 @@ plt.bar(x,height_snapshot[1]-height_snapshot[0], color='red',alpha=0.6,bottom=(h
 plt.bar(x,height_snapshot[2]-height_snapshot[1], color='blue',alpha=0.6,bottom=(height_snapshot[1]))
 plt.bar(x,height_snapshot[3]-height_snapshot[2], color='red',alpha=0.6,bottom=(height_snapshot[2]))
 plt.title("deposition with 20000 samples")
-plt.show()
+plt.show()'''
+
+#now lets work on fits
+#we stil have x and h according to arbitrary L=200
+L=200
+x_co=np.arange(0,L,1)
+h=np.zeros(L)
+#number of samples
+N=20000
+t_snap=[100,1000,10000,100000, N-1]
+h_snapshot=[]
+#i will save the h in different times
+for i in range(N):
+    #choose a house
+    r_house=r.randint(0,L-1)
+    #check the neighbors
+    #we have 200 houses, the house at 0 and 200 should be neighbours. for this we have to link 0 and 199 with a periodic boundary.
+    #module cam do this for us easily because it has affect on the two end houses naturally
+    #print(-1%200) is 199 wich is corrrect!
+    #for 199=> 200%200 is 0 which is correct again :)
+    min_height=min(h[r_house],h[(r_house+1)%L],h[(r_house-1)%L])
+    #now lets see this belongs to which house!
+    if h[r_house]==min_height:
+        h[r_house]=h[r_house]+1
+    elif h[(r_house+1)%L]==h[(r_house-1)%L] and min_height==h[(r_house-1)%L]:
+        #choose randomly between them
+        idx= r.choice([(r_house-1)%L, (r_house+1)%L]) 
+        h[idx]=h[idx]+1
+    elif h[(r_house-1)%L]==min_height:
+        h[(r_house-1)%L]=h[(r_house-1)%L]+1
+    else:
+        h[(r_house+1)%L]=h[(r_house+1)%L]+1
+        
+    
+    #now lets save snap shots!
+    if i in t_snap:
+        h_snapshot.append(h.copy())
+#print(h_snapshot)
+
+
 
