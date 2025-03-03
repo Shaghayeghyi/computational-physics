6716a9a26f7d66e1414e2e2459b700563695cf79
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-03 09:05:24 +0330

    it worked!

--- HW2/Question 2/payin.py
+++ HW2/Question 2/payin.py
@@ -45,6 +45,8 @@ plt.bar(x,height_snapshot[2]-height_snapshot[1], color='blue',alpha=0.6,bottom=(
 plt.bar(x,height_snapshot[3]-height_snapshot[2], color='red',alpha=0.6,bottom=(height_snapshot[2]))
 plt.title("deposition with 20000 samples")
 plt.show()'''
+
+
 #put it all in a function so we can call it for averaging
 def Simulation(N,L):
     #now lets work on fits
@@ -52,17 +54,13 @@ def Simulation(N,L):
     x_co=np.arange(0,L,1)
     h=np.zeros(L)
     #number of samples
-    ''' #creating good time spacing like other reports:)
-    while True:
-        tn = int(1000 * (1.2) ** n) 
-        if tn >= N:  
-            break
-        t_snap.append(tn)
-        n=n+1'''
+    #creating good time spacing like other reports:)
     #more efficient version
     #how many time ranges you want?
-    stop=35
-    t_snap=np.array([N/(1.2**n) for n in range(stop,-1,-1)],int)
+    stop=45
+    t_snap=np.array([N/(1.2**n) for n in range(stop,0,-1)],int)
+    #better spacing may help :"
+    #t_snap =np.unique(np.round(np.logspace(1, np.log10(N), stop)).astype(int))
 
     h_snapshot=[]
     #i will save the h in different times
@@ -76,11 +74,11 @@ def Simulation(N,L):
         #print(-1%200) is 199 wich is corrrect!
         #for 199=> 200%200 is 0 which is correct again :)
         #more efficient no need for % for the last one
-        min_height=min(h[r_house],h[(r_house+1)%L],h[(r_house-1)])
+        min_height=min(h[r_house],h[(r_house+1)%L],h[(r_house-1)%L])
         #now lets see this belongs to which house!
         if h[r_house]==min_height:
             h[r_house]+=1
-        elif h[(r_house+1)%L]==h[(r_house-1)%L]:
+        elif h[(r_house+1)%L]==h[(r_house-1)%L]==min_height:
             #choose randomly between them
             idx= r.choice([(r_house-1)%L, (r_house+1)%L]) 
             h[idx]+=1
@@ -95,69 +93,69 @@ def Simulation(N,L):
             h_snapshot.append(h.copy())
     #print(h_snapshot)
     #now lets calculate the mean and var
-    average_snap=[]
-    w=[]
-    for n in range(len(t_snap)):
-        #mean :)
-        average_snap.append(np.mean(h_snapshot[n]))
-        #more efficient var
-        w.append(np.sqrt(np.var(h_snapshot[n])))
-
+            #maybe more efficient:(
+    average_snap = [np.mean(snap) for snap in h_snapshot]
+    w = [np.sqrt(np.var(snap, ddof=1)) for snap in h_snapshot]
+    
     
     
         
-    return t_snap, w, average_snap
+    return w, average_snap
 #okay now lets test out code for w and
-print(Simulation(40000,200))
+#print(Simulation(40000,200))
+all_data=[]
+#how many runs:
+j=25
 
-'''Var_all = []
-
-for i in range(2):
-    sim = Simulation(1000000,200)
-    Var_all.append(sim[1])
-#averaging over all runs
-meaning=np.mean(Var_all, axis=0)
-#b,a= np.polyfit(np.log(time_snapshot), np.log(w), 1)
+for k in range(j):
+    wk,meank=Simulation(4000000,400)
+    print(k)
+    all_data.append(wk)
     
-t=sim[0]
-mean_w = np.mean(Var_all, axis=0)
-log_t = np.log(t)
-log_w = np.log(meaning)
-'''
+'''print("data 1")
+t_shot, W1, av1=Simulation(4000000,200)
+print("data 2")
+t_shot, W2, av2=Simulation(4000000,200)
+print("data 3")
+t_shot, W3, av3=Simulation(4000000,200)
+print("data 4")
+t_shot, W4, av4=Simulation(4000000,200)
+#now we have 4 data set we just need to average them and plot to lines for them
+all_data = np.array([W1, W2, W3, W4])'''
+stop=45
+t_shot=np.array([4000000/(1.2**n) for n in range(stop,0,-1)],int)
+# Compute the average across all datasets
+average_W = np.mean(all_data, axis=0)
 
-'''split_index = int(0.9 * len(log_t))  
+#we want to plot in the lograrithm scale
+#we could do this or just plot and log log diagram
+log_t = np.log(t_shot)
+log_w = np.log(average_W)
 
-
+#we kinda are able to see the saturation point so we can split our data in two different parts according to that
+split_index = int(0.65* len(log_t))  
+#we wll fit a line to each of these to phases
 slope1, intercept1, _, _, _ = linregress(log_t[:split_index], log_w[:split_index])
 line1 = slope1 * log_t + intercept1
-
+#btw we do not need things  such as regression and error right now so we ignore them basically!
 slope2, intercept2, _, _, _ = linregress(log_t[split_index:], log_w[split_index:])
 line2 = slope2 * log_t + intercept2
 
-
+#just let lines connect and find the critical point
 critical_log_t = (intercept2 - intercept1) / (slope1 - slope2)
 critical_log_w = slope1 * critical_log_t + intercept1
-
-critical_t = np.exp(critical_log_t)
-critical_w = np.exp(critical_log_w)
-
+#we need this out nex plot and report for alpha
+print(slope1, "is beta")
+print(critical_log_w ,"is ln Ws")
 # Plot results
 plt.scatter(log_t, log_w, marker='o', label="Data")
-plt.plot(log_t, line1, 'r--', label="Growth Phase Fit")
-plt.plot(log_t, line2, 'g--', label="Saturation Phase Fit")
-plt.scatter(critical_log_t, critical_log_w, color='black', marker='x', s=100, label="Critical Point")
+#plotting with limit is a better visiulization imo :)
+plt.plot(log_t[:split_index], line1[:split_index], 'r--', label="Growth Phase Fit")
+plt.plot(log_t[split_index:], line2[split_index:], 'g--', label="Saturation Phase Fit")
 plt.xlabel("log t")
 plt.ylabel("log w")
-plt.title("Growth and Saturation Phases of Roughness (Fixed)")
 plt.legend()
 plt.show()
 
-critical_t, critical_w
-plt.scatter(np.log(t), np.log(meaning), marker='o')
-plt.title("w and t for 200000 samples")
-plt.xlabel("logt")
-plt.ylabel("logw")
-plt.show()'''
-            
 
 
