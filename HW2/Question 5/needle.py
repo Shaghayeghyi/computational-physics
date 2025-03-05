a86aab74a063b2c0527db9c01268a9fe599dee38
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-05 19:09:01 +0330

    update of algorithm

--- HW2/Question 5/needle.py
+++ HW2/Question 5/needle.py
@@ -14,7 +14,8 @@ height = np.zeros(L)
 horizantal_shift=np.round(np.sin(deg))
 vertical_shift=np.round(np.cos(deg))
 print(horizantal_shift,vertical_shift)'''
-collide=False
+
+'''collide=False
 for i in range(N_samples):
     #choose a house initially
     r_house= r.randint(0, L - 1)
@@ -22,6 +23,7 @@ for i in range(N_samples):
     moving_sys = [(r_house - k) % L for k in range(1, L)]
 
     for j in moving_sys:
+        j=()
         #check every house on the left
         #assume the particle falls leftward
         #we must check the height of every single room at the left of the index
@@ -50,4 +52,49 @@ plt.bar(x , height,width=1)
 plt.xlabel('Position')
 
 plt.ylabel('Height')
+plt.show()'''
+import numpy as np
+import matplotlib.pyplot as plt
+import random as r
+#we will define the surface at first
+N_samples=1000
+L = 100
+x = np.arange(0,L,1)
+#we can have a 2D array too
+height_map = np.zeros((2000, L))
+#we need to save the height
+height = np.zeros(L)
+#now lets go through samples
+'''deg=np.radians(45)
+horizantal_shift=np.round(np.sin(deg))
+vertical_shift=np.round(np.cos(deg))
+print(horizantal_shift,vertical_shift)'''
+collide=False
+for i in range(N_samples):
+    #choose a house initially
+    r_house= r.randint(0, L - 1)
+    #initial height
+    height1=1000
+    #now we work on the dynamic of each particle
+    while height1>0:
+        #starting point r_house
+       
+        if height1<=height[r_house]:
+            height[r_house]+=1
+            #it doesnt happen at the first step
+            break
+        #move leftward
+        r_house=(r_house-1)%L
+        #decrease the height
+        height1=height1-1
+        if height1==0:
+            #no collision just put it one the surfaace
+            height[r_house]+=1
+       
+plt.figure(figsize=(10, 5))
+plt.bar(x , height,width=1)
+plt.xlabel('Position')
+
+plt.ylabel('Height')
 plt.show()
+    
