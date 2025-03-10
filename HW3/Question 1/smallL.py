4d627df4b8d68a787ff789ae400f22848112f299
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-10 13:58:03 +0330

    start to look for paths

--- HW3/Question 1/smallL.py
+++ HW3/Question 1/smallL.py
@@ -16,5 +16,20 @@ def random_on(L,p):
                 Lattice[i,j]=0
     return Lattice
             
-plt.imshow(random_on(2,0.5))
-plt.show()
+'''plt.imshow(random_on(2,0.5))
+plt.show()'''
+#for seeing whether the percolation happend or not, we will start from on sites and the left
+#for each one we will look at the top, down and right neighbor for a path
+#after visiting each site, we will marke it to not go through that again
+#by moving to right and reaching the very right column with number one, we will assume that the percolation happened
+
+def find_path(Lattice):
+    L=3
+    p=0.5
+    left_column=random_on(L,p)[:,0]
+    #not seen sites are zero
+    paths=np.zeros((L, L), dtype=int)
+    for i in range(L):
+        for j in range(L):
+            
+    
