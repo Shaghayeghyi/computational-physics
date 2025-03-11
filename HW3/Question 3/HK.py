6c07299c4159ee9cb90c6afe5999e9b3cbd90b14
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-11 19:17:20 +0330

    pseudo code in my mind

--- HW3/Question 3/HK.py
+++ HW3/Question 3/HK.py
@@ -8,21 +8,40 @@ from random import random,randint
 #we should create a function that leads us to the root:)
 L=10
 p=0.3
-plane = np.zeros((L,L+2),dtype = int)#L+2 for side's column
-plane[:,0]=1
-plane[:,-1]=100
-count=0
-for j in range(1,L):
-    for i in range(1,L):
-        random_p=random()
-        if random_p<p:
-            plane[i,j]=1
-            count=count+1
-            #creating the grid and counting the number of on sites
-#print(plane)
-        
 
-
-
-
-
+def Plane(L,p):
+    plane = np.zeros((L,L+2),dtype = int)#L+2 for side's column
+    plane[:,0]=1
+    plane[:,-1]=100
+    count=0
+    k=3
+    for j in range(1,L):
+        for i in range(1,L):
+            random_p=random()
+            if random_p<p:
+                plane[i,j]=1
+            #if the site is on
+            if plane[i,j]!=0:
+                left_n=plane[i,j-1]
+                top_n=plane[i-1,j]
+                #if none of them are on
+                if top_n==0 and left_n==0:
+                    #we need a new number
+                    plane[i,j]=k
+                    #the size of cluster with number k increases
+                    cluster_size[k]=1
+                    k=k+1
+                if top_n!=0 and left_n==0:
+                    plane[i,j]=top_n
+                    cluster_size[top_n]+=1
+                if top_n==0 and left_n!=0:
+                    plane[i,j]=left_n
+                    cluster_size[left_n]+=1
+                else:
+                    label=max(top_n, left_n)
+                    plane[i,j]=label
+                    
+                    
+                    
+                    
+            
