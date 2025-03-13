18e0c408d0d3ed36975a9542f87039456a9d79c3
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-13 10:31:28 +0330

    averaging and plotting for one lenght

---
+++ HW3/Question 4/Q.py
@@ -0,0 +1,150 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from random import random,randint
+#we need the pervoius functions
+
+def Plane(L,p):
+    plane = np.zeros((L+2,L+2),dtype = int)#L+2 for side's column
+    plane[:,0]=1
+    plane[:,-1]=100
+    count=0
+    for j in range(1,L+1):
+        for i in range(1,L+1):
+            random_p=random()
+            if random_p<p:
+                plane[i,j]=1
+                count=count+1
+    return plane,count
+#the plane is correct
+#print(Plane(10,0.5))
+
+
+def HK(plane):
+    grid,count=plane
+    #grid=grid[1:-1,1:-1]
+    #print("hello")
+    #print(grid)
+    n_rows, n_columns = grid.shape
+    L=n_rows-2
+    #create a grid to put the labels insdie according to grid
+    label_grid = np.zeros((n_rows, n_columns), dtype=int)
+    label_grid[:,0]=1
+    label_grid[:,-1]=100
+    label_counter=1
+    labels = np.arange(count+2,dtype=int)
+    #i should also add an array to save the size of clusters
+    cluster_size = np.zeros(L * L)
+    
+    def find_root(label,array):
+        if label == labels[label]:
+            return label
+        else:
+            array[label] = find_root(array[label],array)
+            return array[label]
+
+    
+    for j in range(1,L+1):
+        for i in range(1,L+1):
+            #we look at a square grid and don't get out of bound
+            #look for on sites
+            if grid[i,j]!=0:
+                
+                #look for its left and top neighbor
+                top_n=label_grid[i-1,j]
+                left_n=label_grid[i,j-1]
+                #now we have three ways
+                
+                if top_n==0 and left_n==0:
+                    #new cluster
+                    
+                    label_grid[i,j]=label_counter
+                    labels[label_counter] = label_counter
+                    cluster_size[label_counter] += 1
+                    label_counter+=1
+                    
+                    
+                elif top_n==0 and left_n!=0:
+                    #link both to root
+                    label_grid[i,j-1]=find_root(left_n,labels)
+                    label_grid[i,j]=find_root(left_n,labels)
+                    cluster_size[find_root(left_n,labels)] += 1
+                    
+
+                elif top_n!=0 and left_n==0:
+                    #link both to root
+                    label_grid[i-1,j]=find_root(top_n,labels)
+                    label_grid[i,j]=find_root(top_n,labels)
+                    cluster_size[find_root(top_n,labels)] += 1
+                    
+
+                elif top_n!=0 and left_n!=0:
+                    #compare labels
+                    root_L=find_root(left_n,labels)
+                    root_T=find_root(top_n,labels)
+                    label_grid[i - 1][j] = root_T
+                    label_grid[i,j - 1] = root_L
+                    label_grid[i,j] = root_L
+                    
+                    if root_L==root_T:
+                        cluster_size[root_L] +=1
+                    
+                    if root_L != root_T:
+                        #unite roots
+                        #join two clusters and ignore the top one
+                        cluster_size[root_L] = cluster_size[root_L] + cluster_size[root_T] + 1
+                        cluster_size[root_T]=0
+                        labels[root_T] = root_L
+                        
+                        
+   
+    for j in range(1,L+1): 
+        for i in range(1,L+1):
+            label_grid[i, j] = find_root(label_grid[i, j],labels)
+            
+    return label_grid, cluster_size, labels
+                    
+                    
+im,cluster_s,label=HK(Plane(100,0.4))   
+
+def if_percolation(im):
+    first_column=im[:,1]
+    last_column=im[:,-2]
+    #print(last_column)
+    for i in last_column:
+        for j in first_column:
+            if i==j and i!=0:
+                return 1
+    return 0
+
+'''print(im)
+#print(im[:-1,:-1])
+plt.imshow(im[:-1,:-1])  
+print(f'did we have percolation?{if_percolation(im)}')
+plt.title(f"this is for L={L} and p={p}")
+plt.show()
+print(cluster_s)'''
+
+#now i want to use HK algorithm to work on the probability of percolation
+array= []
+averag= []
+p_range = np.arange(0,1.05, 0.05)
+for p in p_range:
+    #run this 100 time for each p
+    #this is for L=10
+    
+    for i in range(100):
+        sample_grid,_,_=HK(Plane(10,p))
+        array.append(if_percolation(sample_grid))  
+    averag.append(np.average(array))
+    array = []
+    
+plt.plot(p_range,averag,  label='this is L = 10')
+plt.xlabel("probability")
+plt.ylabel("Q")
+plt.legend()
+plt.show()
+
+                    
+                    
+                    
+            
