c15aa4d3439cee9689a214c48d2f452b77297f05
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-15 10:28:33 +0330

    change of plans

--- HW3/Question 8/clusterD.py
+++ HW3/Question 8/clusterD.py
@@ -5,33 +5,43 @@ L=100
 p=0.6
 #we will start by a plane and a seed in the middle
 #-1 means off and not visited
-plane = np.full((L,L),-1,dtype = int)
-middle=int(L/2)
-#turn it on
-activation=(middle,middle)
-plane[middle,middle]=1
-
-neighbors=[activation]
-null=[]
-
-
-while neighbors!=null:
+def clusterD(L,p)
+    #at first everything is unvisited
+    plane = np.full((L+2,L+2),-2,dtype = int)
+    #boundary
+    plane[0, :] = 0
+    plane[-1, :] = 0 
+    plane[:, 0] = 0
+    plane[:, -1] = 0
     
-    i,j=neighbors.pop()
-    all_four_sides=[(i-1,j),(i,j-1),(i+1,j),(i,j+1)]
-    #we will only work with on sites
-    for k,l in all_four_sides:
-        random_p=random()
-        if random_p<p and 0<k<L and 0<l<L:
-            if plane[k,l]!=0:
-                plane[k,l]=1
-                neighbors.append((k,l))
-        elif 0<k<L and 0<l<L:
-            #block
-            plane[k,l]=0
-        else:
-            continue
-plt.imshow(plane)
+    middle=int(L/2)
+    #turn it on
+    activation=(middle,middle)
+    plane[middle,middle]=1
+    #the size of cluster is 1 now
+    cluster_size=1
 
-plt.show()
+    current_active=[activation]
+    null=[]
+    all_actives_list=[activation]
+
+
+    while current_active!=null:
+        #find the not visited neighbors at first
+        
+        neighbors = []
+        for row, col in current_active:
+            
+            if plane[row + 1, col] == -2:
+                neighbors.append((row + 1, col))
+            if plane[row - 1, col] == -2:
+                neighbors.append((row - 1, col))
+            if plane[row, col + 1] == -2:
+                neighbors.append((row, col + 1))
+            if plane[row, col - 1] == -2:
+                neighbors.append((row, col - 1))
+
+            
+
+    plt.show()
 
