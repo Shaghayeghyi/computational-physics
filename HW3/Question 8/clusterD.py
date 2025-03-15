ed515128862efffd9f95de1577f46404f236c5cb
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-15 06:04:43 +0330

    extending the idea:)

--- HW3/Question 8/clusterD.py
+++ HW3/Question 8/clusterD.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from random import random,randint
-
+L=100
+p=0.6
 #we will start by a plane and a seed in the middle
-plane = np.zeros((L,L),dtype = int)
+#-1 means off and not visited
+plane = np.full((L,L),-1,dtype = int)
 middle=int(L/2)
 #turn it on
 activation=(middle,middle)
@@ -19,6 +21,17 @@ while neighbors!=null:
     all_four_sides=[(i-1,j),(i,j-1),(i+1,j),(i,j+1)]
     #we will only work with on sites
     for k,l in all_four_sides:
-        
-    
+        random_p=random()
+        if random_p<p and 0<k<L and 0<l<L:
+            if plane[k,l]!=0:
+                plane[k,l]=1
+                neighbors.append((k,l))
+        elif 0<k<L and 0<l<L:
+            #block
+            plane[k,l]=0
+        else:
+            continue
+plt.imshow(plane)
+
+plt.show()
 
