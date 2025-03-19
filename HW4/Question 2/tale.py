9344b41b39d17f78d261eecd343934d8d002e961
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-19 14:12:39 +0330

    average run for various start positions

--- HW4/Question 2/tale.py
+++ HW4/Question 2/tale.py
@@ -20,3 +20,15 @@ def tale(p,position):
             x=x-1
             death_time=death_time+1
     return death_time
+
+run=1000
+mean_result=[]
+for j in range(-10,11):
+    run_result=[]
+    for i in range(run):
+        p=0.5
+        TD=tale(p, j)
+        run_result.append(TD)
+    mean_result=np.mean(run_result)
+    
+
