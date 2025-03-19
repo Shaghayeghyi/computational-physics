02d8855e37b3cdac2e5d49236171bc93a6db39bb
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-19 09:14:58 +0330

    average running and plot

--- HW4/Question 1/RW.py
+++ HW4/Question 1/RW.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from random import randint,random
 
 #simulating random walk
-def random_walk(P)
+def random_walk(P):
 
     p=P
     q=1-p
@@ -13,7 +13,7 @@ def random_walk(P)
     positions.append(start_point)
     position_copy=[]
     iteration=1000
-    for i in range(len(iteration)):
+    for i in range(iteration):
         rand = random()
         #right and left
         if rand < p:
@@ -22,16 +22,37 @@ def random_walk(P)
             current_position=current_position-1
             
         positions.append(current_position)
-         if i%100==0: #make a copy
-             position_copy.append(positions.copy())
+        if i%100==0: #make a copy
+            position_copy.append(positions.copy())
              
     averag=[]
     Std=[]
-    for j in range(len(positions_copy))
-        averag.append(np.mean(positions_copy[j]))
-        Std.append(np.std(positions_copy[j]))
+    for j in range(len(position_copy)):
+        averag.append(np.mean(position_copy[j]))
+        Std.append(np.std(position_copy[j]))
     
     return averag, Std
 #average and Std of position
+
+#lets run this code for 100 times
+
+run=100
+run_av=[]
+run_st=[]
+
+for k in range(run):
+    P=0.2
+    averag, Std=random_walk(P)
+    run_av.append(averag)
+    run_st.append(Std)
+
+plt.figure(figsize=(10, 5))
+plt.plot(np.mean(run_av, axis=0), label='Average Position')
+plt.plot(np.mean(run_st, axis=0), label='Std Position')
+plt.xlabel('Iteration (x100)')
+plt.ylabel('Position')
+plt.legend()
+plt.show()
+
    
 
