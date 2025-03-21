a4c457577693eaa053528c8e0568353ad52e7af5
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-21 13:35:18 +0330

    commit and change of coordinate

--- HW4/Question 2/tale.py
+++ HW4/Question 2/tale.py
@@ -9,7 +9,7 @@ def tale(p,position):
     death_time=0
     x=start
 
-    while -10<x<10:
+    while 0<x<20:
 
         rand=random()
 
@@ -21,14 +21,25 @@ def tale(p,position):
             death_time=death_time+1
     return death_time
 
-run=1000
+run=100000
 mean_result=[]
-for j in range(-10,11):
+x_range = np.arange(0, 21)
+for j in x_range:
     run_result=[]
     for i in range(run):
         p=0.5
         TD=tale(p, j)
         run_result.append(TD)
-    mean_result=np.mean(run_result)
+    mean_result.append(np.mean(run_result))
     
+a,b,c = np.polyfit(x_range,mean_result,deg=2)#fit of a degree 2 polynomial ax^2+bx+c
 
+fit=a*(x_range)**2+b*x_range+c
+
+plt.figure(figsize=(10, 5))
+plt.scatter(x_range,mean_result , label='average death time')
+plt.plot(x_range,fit, label=f'fit {a}x^2+{b}x+{c}')
+plt.xlabel('x')
+plt.ylabel('time')
+plt.legend()
+plt.show()
