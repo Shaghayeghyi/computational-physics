fc330c49060c732041b023225b19f6f628bd9b97
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-23 17:50:34 +0330

    defining stick and random walk

--- HW4/Question 5/lineseed.py
+++ HW4/Question 5/lineseed.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 
 #we need to create a plane
 width=200
-height=220
+height=210
+start_height=200
 grid=np.zeros((width,height))
 #initial seed
 grid[-1,:]=1
@@ -15,29 +16,66 @@ grid[-1,:]=1
 
 def random_walk2(current_position):
     
-    position_copy = []
+    rand = random()
 
-    iteration = 100
-    for i in range(iteration + 1):
-        rand = random()
-
-        if rand < 1/4:
-            #right
-            current_position = (current_position[0] + 1, current_position[1])  
+    if rand < 1/4:
+        #right
+        current_position = (current_position[0] + 1, current_position[1])  
             
-        elif rand < 2/4:
+    elif rand < 2/4:
             
-            #up
-            current_position = (current_position[0], current_position[1] + 1)
-        elif rand < 3/4:
+        #up
+        current_position = (current_position[0], current_position[1] + 1)
+    elif rand < 3/4:
             
-            #down
-            current_position = (current_position[0], current_position[1] - 1) 
+        #down
+        current_position = (current_position[0], current_position[1] - 1) 
             
-        else:
+    else:
             
-            #left
-            current_position = (current_position[0] - 1, current_position[1])  
+        #left
+        current_position = (current_position[0] - 1, current_position[1])  
 
             
     return current_position
+
+#i want the RW continue till it gets stuck or gets out of range
+
+#i need a function that checks all the neighbors of the target site to see whether a merging will happen.
+
+
+def will_it_merge(x,y):
+    four_moves=[(1,0),(0,1),(-1,0),(0,-1)]
+    for m,n in four_moves:
+        xneighbor=x+m
+        yneighbor=y+n
+        if grid[xneighbor, yneighbor]!=0
+            return 1 #yes it will stick
+    return 0 #no i did not stick
+            
+        
+#test particle
+x_start=random.randint(0, width-1)
+y_start=start_height
+x=x_start
+y=y_start
+color=10                
+while True:
+    #start walking
+    x,y +=random_walk2((x,y))
+
+    if will_it_merge:
+        grid[x,y]=color
+        
+    elif not will_it_merge:
+        break
+    
+
+    
+
+    
+    
+
+
+
+    
