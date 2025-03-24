34c26969904fe69aafda864d9ebf20c7c9fb7643
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-24 16:17:14 +0330

    update the structure of funtions

--- HW4/Question 6/centralseed.py
+++ HW4/Question 6/centralseed.py
@@ -3,15 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #we need to create a plane
-width=200
-height=100
+width=210
+height=210
 grid=np.zeros((height,width))
 #initial seed is in center now
 x_c=int(width/2)
 y_c=int(height/2)
 grid[int(height/2),int(width/2)]=1
 #print(grid)
-maxr=12
+maxr=100
 def random_walk2(x,y):
     
     rand = random.random()
@@ -41,29 +41,31 @@ def will_it_merge(x,y, grid):
     
     four_moves=[(1,0),(0,1),(-1,0),(0,-1)]
     for m,n in four_moves:
-        xneighbor=(x+m)
+        xneighbor=x+m
         yneighbor=y+n
-        if 0 <= (y_neighbor**2+ x_neighbor**2)< maxr**2 and grid[y_neighbor, x_neighbor] != 0:
-            return True #yes it will stick
+        if 0 <= xneighbor < width and 0 <= yneighbor < height:
+             if grid[yneighbor, xneighbor] != 0:
+                return True #yes it will stick
+             
     return False #no i did not stick
             
         
 #test particles
-particles=200
+particles=2000
 def initial_circle(x_c,y_c,radius):
     initials=[]
     #number of positions
-    num=50
+    num=100
     angles = np.linspace(0, 2 * np.pi, num, endpoint=False)
-    x_on_circle= x_c + radius * np.cos(angles) 
-    y_on_circle = y_c + radius * np.sin(angles) 
+    x_on_circle= x_c + np.round(radius * np.cos(angles)).astype(int) 
+    y_on_circle = y_c + np.round(radius * np.sin(angles)).astype(int)
     #initials.append(( x_on_circle ,y_on_circle))
     return list(zip(x_on_circle, y_on_circle))
 
 
 for i in range(particles):
     #randomly choose on a circle with radius r
-    radius=10
+    radius=95
     x_start, y_start=random.choice(initial_circle(x_c,y_c,radius))
     x=x_start
     y=y_start
@@ -75,7 +77,7 @@ for i in range(particles):
         x,y=random_walk2(x,y)
     
     
-        if (x**2+y**2)>maxr**2:
+        if ((x-x_c)**2+(y-y_c)**2)>maxr**2:
             x_start, y_start=random.choice(initial_circle(x_c,y_c,radius))
             x=x_start
             y=y_start
@@ -91,11 +93,12 @@ for i in range(particles):
 
 '''plt.imshow(grid, cmap='nipy_spectral') 
 plt.title('DLA Simulation with Initial Line Seed')
-plt.show()  '''
+plt.show()'''
+
 
 plt.pcolor(grid , cmap ='nipy_spectral')
 
-    
+   
     
 
 
