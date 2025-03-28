51ab37e617927146e1c7e09c79bb100fc7a84e21
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-28 07:45:30 +0330

    correcting the logic, copy branches

--- HW4/Question 8/SAW1.py
+++ HW4/Question 8/SAW1.py
@@ -4,20 +4,33 @@ import matplotlib.pyplot as plt
 
 #i want to find every possible SAW with a lenght N and plot the results
 #we will make it recursive and the output will be the count
-def all_SAW(N,x,y):
+all_visited=[]
+#at first we have not visited anywhere
+def all_SAW(N,x=0 ,y=0 , visited_sites=None):
+    
     count=0
-    # i will create a grid
-    plane=np.zeros((2*N,2*N))
+    if visited_sites is None:
+        visited_sites=[(x,y)]
+    #plane=np.zeros((2*N,2*N))
+    
     if N==0:
-        return 1 #only one way possible
+        count=1
+        return count #you can say one way possible
+        
     else:
-        plane[x,y]=1 #visited
+        #plane[x,y]=1 #visited
         shifts=[(x+1,y),(x,y+1),(x-1,y),(x,y-1)]
         #if the neighbors are not vistied then we need to check all of them
         for shift in shifts:
             new_posx, new_posy=shift
-            if plane[new_posx,new_posy]!=1: #not visited
-                count+=1
+            if shift not in visited_sites: #not visited
+                new_visit=visited_sites.copy()
+                new_visit.append((new_posx,new_posy))
+                #plane[new_posx,new_posy]=1
+                sub_count=all_SAW(N-1,new_posx,new_posy, new_visit)
+                count+=sub_count
                 
-
+    return count
+    
+print(all_SAW(5,0,0,None))             
     
