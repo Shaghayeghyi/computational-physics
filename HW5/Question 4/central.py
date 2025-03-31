6026e52a67f1ca20ef62eb7e545875e1363b29ae
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-03-31 17:05:12 +0330

    plot and update

--- HW5/Question 4/central.py
+++ HW5/Question 4/central.py
@@ -1,24 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from random import randint,random
+from random import randint
 
 #central limit theorem
 def central_limit(N):
-    
-    runs=10000
-    #create gaussian variables
+    runs = 100000
+    G_data =[]
     for run in range(runs):
-        G_data=[]
-        gaussian_var=0
-        
-        for n in range(N):
-            random_number=randint(0,9)
-            gaussian_var+=random_number
-            
-        G_data.append(gaussian_var)
-
+        #use built in functions
+        #gaussian variable
+        y =np.sum(np.random.randint(10, size=N))/N
+        G_data.append(y)
     return G_data
-
-
-
-    
+Ns=[5]
+for N in Ns:
+    ys=central_limit(N)
+    plt.hist(ys, bins=50 ,alpha=0.7, rwidth=0.85 , density=True)
+    plt.xlabel("Average Value")
+    plt.ylabel("Probability Density")
+    plt.title(f'Distribution of sum of {N} Variables')
+    plt.show()
