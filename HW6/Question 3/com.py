c95583dd1ca93b0e469b3b62056461f9571ca540
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-04-09 16:26:07 +0330

    initial center of mass function

---
+++ HW6/Question 3/com.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from random import randint,random
+
+def center_of_mass(N,R):
+
+    for i in range(N):
+    
+        #generate 3 random numbers
+        x = np.random.uniform(-R, R)
+        y = np.random.uniform(-R, R)
+        z = np.random.uniform(-R, R)
+
+        
+
+        
+
+    
+
+     
+    
