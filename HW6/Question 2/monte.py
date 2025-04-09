328f99ad3cd818689e9401de5f8ed3df5b6108fa
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-04-09 14:12:16 +0330

    add important sampling

--- HW6/Question 2/monte.py
+++ HW6/Question 2/monte.py
@@ -21,7 +21,7 @@ result=real_result()
 def simple(N):
 
     sample=[]
-
+    start=time()
     for i in range(N):
 
         random_x=np.random.uniform(0,2)
@@ -31,11 +31,53 @@ def simple(N):
 
     integral=2*np.mean(sample)
     #print(integral)
-
+    
+    stop=time()
+    
     #for the error we need to calculate the std over sqrt(N)
     sample_error=np.std(sample)/np.sqrt(N)
     print(sample_error)
 
+    #we should calculate the real error too
+
+    real_error=real_result()-integral
+    print(real_error)
+
+    #let's see how much time it take
+
+    duration=stop-start
+    print(duration)
+
     
 
-simple(1000)
+def important(N):
+
+    sample=[]
+    start=time()
+    for i in range(N):
+        #just chane the generator
+        random_x=-np.log(np.random.uniform(np.exp(-2), 1))
+        important_y=np.exp(-random_x**2)/np.exp(-random_x)
+        sample.append(important_y)
+    #so we have our sample now we need to calculate the mean
+
+    integral=(1-np.exp(-2))*np.mean(sample)
+    print(integral)
+    
+    stop=time()
+    
+    #for the error we need to calculate the std over sqrt(N)
+    sample_error=np.std(sample)/np.sqrt(N)
+    print(sample_error)
+
+    #we should calculate the real error too
+
+    real_error=real_result()-integral
+    print(real_error)
+
+    #let's see how much time it take
+
+    duration=stop-start
+    print(duration)
+    
+important(10000)
