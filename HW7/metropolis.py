770c0885f5d73e4c7f2f1be3253d3da427fe94d6
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-04-16 14:29:58 +0330

    correlation definition

--- HW7/metropolis.py
+++ HW7/metropolis.py
@@ -34,12 +34,38 @@ def metropolic(size,step):
             
 #print(metropolic(1000,0.1))
 
-step=0.5
+
+step=0.35
 rate, array=metropolic(1000000,step)
-print(f"this is the rate of acceptance:{rate}")
-plt.hist(array, bins=70, alpha=0.7,rwidth=0.85, density=True)
+print(f"this is the rate of acceptance:{rate} for step={step}")
+'''plt.hist(array, bins=70, alpha=0.7,rwidth=0.85, density=True)
 plt.title(f"gaussian histogram for step={step}")
-plt.show()
+plt.show()'''
+
+#for various js from 0 to 100
+def correlation(array):
+    n=len(array)
+    sigma=np.var(array)
+    correlation_js=[]
+    for j in range(100):
+        sum_ij=0
+        sum_j=0
+        for i in range(n-j):
+            sum_ij+=array[i]*array[i+j]
+            sum_j+=array[i+j]
+        mean_ij=sum_ij/(n-j)
+        mean_j=sum_j/(n-j)
+        lenght_j=(mean_ij-np.mean(array)*mean_j)/sigma
+        correlation_js.append(lenght_j)
+
+    return correlation_js
+            
+print(correlation(array))        
+
+
+
+
+    
 
         
 
