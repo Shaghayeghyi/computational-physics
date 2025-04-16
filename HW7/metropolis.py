36d8441d325a5d16f8d3b20963673c32329a7f0d
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-04-16 11:42:08 +0330

    define metropolis funciton

--- HW7/metropolis.py
+++ HW7/metropolis.py
@@ -4,13 +4,48 @@ import matplotlib.pyplot as plt
 import random
 
 #assume the equilibrium
-def Gaussian_p(x,mean,sigma):
+def Gaussian_p(x):
 
-    g=(1/np.sqrt(2*np.pi*sigma))*np.exp(-(x-mean)**2/(2*sigma**2))
+    g=np.exp(-(x)**2)
     return g
 
 
 def metropolic(size,step):
+    #rate of condition's acceptance
+    accept_count=0
+    x=0
+    number_array=[]
+
+    for loop in range(size):
+        #move
+        y=x+step*np.random.uniform(-1,1)
+        px=Gaussian_p(x)
+        py=Gaussian_p(y)
+
+        if np.random.uniform(0,1)<(py/px):
+            x=y
+            accept_count+=1
+    
+        number_array.append(x)
+
+    rate=accept_count/size
+    return rate, number_array
+
+            
+#print(metropolic(1000,0.1))
+
+step=0.5
+rate, array=metropolic(1000000,step)
+print(f"this is the rate of acceptance:{rate}")
+plt.hist(array, bins=70, alpha=0.7,rwidth=0.85, density=True)
+plt.title(f"gaussian histogram for step={step}")
+plt.show()
+
+        
+
+        
+
+    
     
 
 
