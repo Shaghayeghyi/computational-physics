e04e8cc64e61c622a17725f2b75eb53341db007b
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-04-20 15:24:59 +0330

    define energy function

---
+++ HW8/ising.py
@@ -0,0 +1,53 @@
+#we will try to @jit to make our code faster as Alireza explained :)
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from numba import jit
+
+#creating the initial lattice
+@jit(nopython=True)
+def Lattice(L):
+    #get 0 1, then map to -1 and 1 this is supported by jit
+    rand_array = np.random.randint(0, 2, size=(L, L))
+    lattice = 2 * rand_array - 1
+    return lattice
+#check
+#lattice=Lattice(50)
+
+
+#save the energy change for a filp in (i,j)!
+@jit(nopython=True)
+def delta_E(lattice,i,j,L,J):
+    #boundary
+    delta=2*J*lattice[i,j]*(lattice[(i-1)%L,j]+lattice[(i+1)%L,j]+lattice[i,(j-1)%L]+lattice[i,(j+1)%L])
+    return delta
+#the outputs seeem to be correct according to book
+#print(delta_E(lattice,0,1,10,1))
+
+
+#now let's define the metropolis loops
+def metropolis(lattice,L,J):
+    #we want to check the energy change by going through a random choice of i,j 
+    for iteration in range(L*L):
+        i=np.random.randint(0,L)
+        j=np.random.randint(0,L)
+        dE=delta_E(lattice,i,j,L,J)
+        if dE <= 0 or np.random.rand() < np.exp(-dE):
+            lattice[i, j] *= -1
+            
+    return lattice
+#metropolis(lattice,50,2)
+
+#we need to define functions for energy of lattice, m , Cv , X and correlation lenght
+
+@jit(nopython=True)
+def energy(lattice, L, J):
+    sum_of_neighbors=0
+    for i in range(L):
+        for j in range(L):
+            S_ij=lattice[i,j]
+            sum_of_neighbors+=(S_ij*(lattice[(i-1)%L,j]+lattice[(i+1)%L,j]+lattice[i,(j-1)%L]+lattice[i,(j+1)%L]))
+    energy=-J*(sum_of_neighbors /2)
+    return energy
+            
+            
