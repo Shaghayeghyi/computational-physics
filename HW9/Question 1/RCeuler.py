36f1e92f23677282f0665b675f240863e5bcb494
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-05-06 13:14:01 +0330

    plot real vs euler

--- HW9/Question 1/RCeuler.py
+++ HW9/Question 1/RCeuler.py
@@ -4,22 +4,38 @@ from time import time
 
 
 def euler(h,q,step):
-    time=np.arange(int(step))
+    time=np.zeros(step+1)
     qs=[]
     qs.append(q)
-    for i in time:
+    for i in range(step):
         q=q+h*(1-q)
         qs.append(q)
-        
-        
+        time[i+1]=time[i]+h
 
-        
-    
+    return time , qs
 
+# Parameters
+h = 10**-4
+q0 = 0
+steps = int(5 / h)
+
+T,Q=euler(h,q0,steps)        
 
 
 #define the real answer
-def real_result(t):
+def real_result(time_array):
+    qs = 1 - np.exp(-time_array)
+    return time_array, qs
+    
 
-    return 1-np.exp(-1*t)
+T_real, Q_real =real_result(T)
 
+plt.figure(figsize=(10, 5))
+plt.plot(T, Q, label="Euler", linestyle="--")
+plt.plot(T_real, Q_real, label="Exact", linewidth=2)
+plt.xlabel("Time")
+plt.ylabel("Q(t)")
+plt.title(f"Euler vs Exact for h={h}")
+plt.legend()
+plt.grid(True)
+plt.show()
