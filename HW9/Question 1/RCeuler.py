5aca1cdfe082640b9d311451ef3ee2ee3ef194ab
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-05-11 08:16:28 +0330

    add discharge

--- HW9/Question 1/RCeuler.py
+++ HW9/Question 1/RCeuler.py
@@ -65,3 +65,51 @@ print(f"the slope of the fit is{a}")
 #plt.plot(hs, a*hs+b ,label=f'the slop is {a}')
 plt.grid(True)
 plt.show()
+
+
+
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+
+
+def euler(h,q,step):
+    time=np.zeros(step+1)
+    qs=[]
+    qs.append(q)
+    for i in range(step):
+        q=q+h*(-q)
+        qs.append(q)
+        time[i+1]=time[i]+h
+
+    return time , qs
+
+# Parameters
+h = 10**-4
+q0 = 1
+steps = int(5 / h)
+
+T,Q=euler(h,q0,steps)        
+
+
+#define the real answer
+def real_result(time_array):
+    qs = np.exp(-time_array)
+    return time_array, qs
+    
+
+T_real, Q_real =real_result(T)
+
+plt.figure(figsize=(10, 5))
+plt.scatter(T, Q, s=1 , color="red", label="Euler")
+plt.plot(T_real, Q_real, label="Exact", linewidth=2)
+plt.xlabel("Time")
+plt.ylabel("Q(t)")
+plt.title(f"Euler vs Exact for h={h}")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+'''
+
