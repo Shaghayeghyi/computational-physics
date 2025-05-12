d4866d81253f17dabfbb62c77576f9562b6392f4
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-05-12 16:31:40 +0330

    update plot

--- HW10/Question 1/badalghorithm.py
+++ HW10/Question 1/badalghorithm.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 
 def bad(h,q0):
-    steps = int(7 / h)
+    steps = int(5/ h)
     time = np.zeros(steps + 1)
     qs=[]
     qs.append(q0)
-    #q1=q0+h*(1-q0)
+    q1=q0+h*(1-q0)
     time[0]=0
     time[1]=h
     #or
-    q1=1-np.exp(-h)
+    #q1=1-np.exp(-h)
     qs.append(q1)
     for step in range(1,steps):
         q_new=qs[step-1]+2*(1-qs[step])*h
@@ -29,18 +29,19 @@ def real_result(time_array):
     return time_array, qs
     
 #initial value
-h = 0.1
 q0 = 0
-T, Q = bad(h,q0)
+T1, Q1 = bad(0.1,q0)
+T2, Q2 = bad(0.08,q0)
 
-T_real, Q_real = real_result(T)
+T_real, Q_real = real_result(T1)
 
 plt.figure(figsize=(10, 5))
-plt.plot(T, Q, label="bad!", color="purple", linewidth=1)
+plt.plot(T1, Q1, label=f"bad! h=0.1", color="purple", linewidth=1)
+plt.plot(T2, Q2, label=f"bad! h=0.08", color="yellow", linewidth=1)
 plt.plot(T_real, Q_real, label="exact", linewidth=2)
 plt.xlabel("T")
 plt.ylabel("Q")
-plt.title(f"bad vs Exact for h={h}")
+#plt.title(f"bad vs Exact for h={h}")
 plt.legend()
 plt.grid(True)
 plt.show()
