1297aadb9f0be44f6a69b101cd917314a15122c1
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-05-13 19:05:49 +0330

    add leap frog

--- HW10/Question 2/SHO.py
+++ HW10/Question 2/SHO.py
@@ -28,7 +28,7 @@ def real_sol(h):
     return time, vs, xs
 
 
-#define other sols
+#other solution with cromer
 def euler_cromer(h):
     #i want the time be the same for all of my functions
     #omega=1
@@ -44,12 +44,33 @@ def euler_cromer(h):
         
 
     return time, np.array(vs), np.array(xs)
+
+#leap frog
+def leap(h):
+    #a=-1
+    time = np.arange(0, 60, h)
+    xs = [1]
+    v_half_minus =0
+    vs_full=[0]
+    #we need to use euler for the first step as explained
+    v_half=[-0.5*h*1]
     
+    for n in range(len(time) - 1):
+        v_full_new=vs_full[n]-xs[n]*h
+        vs_full.append(v_full_new)
+        v_half_new = v_half[n]-xs[n]* h
+        v_half.append(v_half_new)
+        x_new = xs[n] + v_half[n + 1] * h
+        xs.append(x_new)
+    
+        
+    return time, np.array(vs_full), np.array(xs)
 
 h=0.01
 T_real, V_real, X_real = real_sol(h)
 T_euler, V_euler, X_euler = euler(h)
 T_cromer, V_cromer, X_cromer =  euler_cromer(h)
+T_leap, V_leap, X_leap = leap(h)
 
 
 plt.figure(figsize=(12, 5))
@@ -58,6 +79,7 @@ plt.subplot(1, 2, 1)
 plt.plot(T_real, X_real, label='exact', linestyle='--')
 plt.plot(T_euler, X_euler, label='euler', alpha=0.7)
 plt.plot(T_cromer, X_cromer, label='cromer', alpha=0.7)
+plt.plot(T_leap, X_leap, label='leap', alpha=0.7)
 plt.xlabel("T")
 plt.ylabel("X")
 plt.legend()
@@ -67,6 +89,7 @@ plt.subplot(1, 2, 2)
 plt.plot(X_real, V_real, label='exact', linestyle='--')
 plt.plot(X_euler, V_euler, label='euler', alpha=0.7)
 plt.plot(X_cromer, V_cromer, label='cromer', alpha=0.7)
+plt.plot(X_leap, V_leap, label='leap', alpha=0.7)
 plt.xlabel("X")
 plt.ylabel("V")
 plt.legend()
