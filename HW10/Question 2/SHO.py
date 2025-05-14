7e173e8e1f216eb9548cbe8f2298dc6498cd0973
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-05-14 10:14:18 +0330

    add verlet and Vverlet, modify leap frog

--- HW10/Question 2/SHO.py
+++ HW10/Question 2/SHO.py
@@ -50,27 +50,70 @@ def leap(h):
     #a=-1
     time = np.arange(0, 60, h)
     xs = [1]
-    v_half_minus =0
-    vs_full=[0]
+    #vs_full=[0]
     #we need to use euler for the first step as explained
-    v_half=[-0.5*h*1]
+    vs_half=[0.5*h*1]
     
     for n in range(len(time) - 1):
-        v_full_new=vs_full[n]-xs[n]*h
-        vs_full.append(v_full_new)
-        v_half_new = v_half[n]-xs[n]* h
-        v_half.append(v_half_new)
-        x_new = xs[n] + v_half[n + 1] * h
+        #v_full_new=vs_full[n]-xs[n]*h
+        #vs_full.append(v_full_new)
+        vs_half_new = vs_half[n]-xs[n]* h
+        vs_half.append(vs_half_new)
+        x_new = xs[n] + vs_half[n + 1] * h
         xs.append(x_new)
+    #an average
+    vs_full = []
+    for i in range(1,len(time)):
+        v_full = (vs_half[i-1] + vs_half[i]) / 2
+        vs_full.append(v_full)
+    vs_full.append(vs_half[-1] - 0.5 * h * xs[-1])
     
         
     return time, np.array(vs_full), np.array(xs)
 
+
+def verlet(h):
+    # a = -x 
+    time = np.arange(0, 60, h)
+    xs = [1] 
+    vs = [0] 
+    #again i need to use euler for derving another value
+    x1 = xs[0] + h * vs[0]
+    xs.append(x1)
+    v1=(xs[1]-xs[0])/(2*h)
+    vs.append(v1)
+    for n in range(1, len(time)-1):
+        x_new = 2*xs[n]-xs[n-1] -xs[n]*h*h
+        xs.append(x_new)
+        v_new = (xs[n+1] - xs[n-1]) / (2 * h)
+        vs.append(v_new)
+    
+    return time, np.array(vs), np.array(xs)
+
+
+def Vverlet(h):
+    # a = -x 
+    time = np.arange(0, 60, h)
+    xs = [1] 
+    vs = [0] 
+    
+    for n in range(len(time)-1):
+        x_new = xs[n]+h*vs[n] -0.5*xs[n]*h*h
+        xs.append(x_new)
+        v_new = vs[n]-0.5*h*(xs[n]+xs[n+1])
+        vs.append(v_new)
+    
+    return time, np.array(vs), np.array(xs)
+
+
+    
 h=0.01
 T_real, V_real, X_real = real_sol(h)
 T_euler, V_euler, X_euler = euler(h)
 T_cromer, V_cromer, X_cromer =  euler_cromer(h)
 T_leap, V_leap, X_leap = leap(h)
+T_verlet, V_verlet, X_verlet = verlet(h)
+T_Vverlet, V_Vverlet, X_Vverlet = Vverlet(h)
 
 
 plt.figure(figsize=(12, 5))
@@ -80,6 +123,8 @@ plt.plot(T_real, X_real, label='exact', linestyle='--')
 plt.plot(T_euler, X_euler, label='euler', alpha=0.7)
 plt.plot(T_cromer, X_cromer, label='cromer', alpha=0.7)
 plt.plot(T_leap, X_leap, label='leap', alpha=0.7)
+plt.plot(T_verlet, X_verlet, label='verlet', alpha=0.7)
+plt.plot(T_Vverlet, X_Vverlet, label='Vverlet', alpha=0.7)
 plt.xlabel("T")
 plt.ylabel("X")
 plt.legend()
@@ -90,6 +135,8 @@ plt.plot(X_real, V_real, label='exact', linestyle='--')
 plt.plot(X_euler, V_euler, label='euler', alpha=0.7)
 plt.plot(X_cromer, V_cromer, label='cromer', alpha=0.7)
 plt.plot(X_leap, V_leap, label='leap', alpha=0.7)
+plt.plot(X_verlet, V_verlet, label='verlet', alpha=0.7)
+plt.plot(X_Vverlet, V_Vverlet, label='Vverlet', alpha=0.7)
 plt.xlabel("X")
 plt.ylabel("V")
 plt.legend()
