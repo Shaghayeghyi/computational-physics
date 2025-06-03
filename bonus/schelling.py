bd0a75d46e6a5c721e401f39f37d7e3970366430
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-06-03 14:12:22 +0330

    add happiness function

--- bonus/schelling.py
+++ bonus/schelling.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+Bm=0.75
 
 def grid():
     N=60
@@ -9,7 +10,6 @@ def grid():
     ratio1=0.4
     ratio2=0.4
     ratio0=0.2
-    Bm=0.75
     values = [0, 1, 2]
     weights = [ratio0, ratio1, ratio2] 
     # Create grid
@@ -17,6 +17,37 @@ def grid():
     return grid
         
 g = grid()
-plt.imshow(g, cmap='coolwarm')
+'''plt.imshow(g, cmap='coolwarm')
 plt.title("Randomly Created Grid")
-plt.show()
+plt.show()'''
+
+
+def happiness(grid, i,j):
+    N=grid.shape[0]
+    agent=grid[i,j]
+    moves=[1,0,-1]
+    #check all possible moore neighbors
+    same=0
+    diff=0
+    for l in [-1, 0, 1]:
+        for m in [-1, 0, 1]:
+            # skip self
+            if l == 0 and m == 0:
+                continue 
+            ni, nj = i + l, j + m
+            if 0 <= ni < N and 0 <= nj < N:
+                neighbor = grid[ni, nj]
+                if neighbor != 0:
+                    if neighbor == agent:
+                        same += 1
+                    else:
+                        diff += 1
+
+    total = same + diff
+    if total == 0:
+        return True
+    #??
+    return (same / (diff+same)) >= Bm
+            
+            
+    
