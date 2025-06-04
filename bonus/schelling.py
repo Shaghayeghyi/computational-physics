fe77bf881e5fc1bde9a7504de9e16ebb6367bccb
Shaghayeghyi <shaghayegh.yavarri@gmail.com>
2025-06-04 21:26:11 +0330

    Average Moves to Equilibrium

--- bonus/schelling.py
+++ bonus/schelling.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-Bm=0.75
+
 
 def grid():
     N=60
@@ -22,7 +22,7 @@ plt.title("Randomly Created Grid")
 plt.show()'''
 
 
-def happiness(grid, i,j):
+def happiness(grid, i,j, Bm):
     N=grid.shape[0]
     agent=grid[i,j]
     moves=[1,0,-1]
@@ -58,7 +58,7 @@ def find_unhappy(grid):
         for j in range(N):
             if grid[i,j]==0:
                 empty.append((i,j))
-            elif not happiness(grid, i,j):
+            elif not happiness(grid, i,j, Bm):
                 unhappy.append((i,j))
 
     return empty, unhappy
@@ -80,21 +80,41 @@ def move_agents(grid, empty, unhappy):
 
 
 def simulate(grid):
-    max_steps=200
+    max_steps=500
     history = [grid.copy()]
-
+    moves=0
     for step in range(max_steps):
         empty, unhappy = find_unhappy(grid)
         if not unhappy:
-            print(f"Equilibrium reached at step {step}")
+            #print(f"Equilibrium reached at step {step}")
             break
         grid = move_agents(grid, empty, unhappy)
         history.append(grid.copy())
+        moves+=1
     
-    return history
+    return history, moves
 
 
-from matplotlib.animation import FuncAnimation, PillowWriter
+Bms=np.linspace(0.1, 1.0, 10)
+g=grid()
+moving=[]
+for Bm in Bms:
+    means=[]
+    for i in range(8):
+            G=g.copy()
+            _, moves=simulate(G)
+            means.append(moves)
+    moving.append(np.mean(means))
+    
+
+plt.plot(Bms, moving, marker='o', linestyle='-', color='b')
+plt.xlabel("Bm")
+plt.ylabel("Average Moves to Equilibrium")
+plt.grid(True)
+plt.show()
+
+
+'''from matplotlib.animation import FuncAnimation, PillowWriter
 from IPython.display import HTML
 
 # Create and simulate the grid
@@ -114,4 +134,4 @@ ani = FuncAnimation(fig, update, frames=len(history), interval=300, blit=True)
 ani.save("schelling_model.gif", writer="pillow", fps=5, dpi=100)
 plt.close() 
 HTML(ani.to_jshtml()) 
-#use plt.show() if not in jupyter
+#use plt.show() if not in jupyter'''
